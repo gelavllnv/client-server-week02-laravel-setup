30db0806a8a101d092d07738949c7f6d2a15b874
gelavllnv <maryangelavillanueva0795@gmail.com>
2026-08-08 14:45:44 +0800

    feat: initialize Laravel project

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Hello Laravel</title>
+    <style>
+        body {
+            font-family: 'Segoe UI', Arial, sans-serif;
+            background: linear-gradient(135deg, #f8f9fa, #e9ecef);
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            margin: 0;
+        }
+        .card {
+            background-color: #fff;
+            padding: 40px;
+            border-radius: 12px;
+            box-shadow: 0 4px 20px rgba(0,0,0,0.1);
+            width: 500px;
+            text-align: center;
+        }
+        h1 {
+            color: #dc3545;
+            margin-bottom: 10px;
+        }
+        p {
+            margin: 6px 0;
+            color: #333;
+        }
+        .label {
+            font-weight: bold;
+            color:rgb(78, 9, 92);
+        }
+    </style>
+</head>
+<body>
+    <div class="card">
+        <h1>Hello Laravel!</h1>
+        <p>Welcome to Client‑Server Technologies.</p>
+        <p><span class="label">Student Name:</span> Mary Angela Villanueva</p>
+        <p><span class="label">Student Number:</span> 0124-1185</p>
+        <p><span class="label">Course:</span> Bachelor of Science in Information Technology</p>
+        <p><span class="label">Section:</span> WAMD - 3A</p>
+        <p><span class="label">Subject:</span> ITST 302 – Client‑Server Technologies</p>
+        <p span class="label">Current Date:</span> August 8, 2026</p>
+    </div>
+</body>
+</html>
+
